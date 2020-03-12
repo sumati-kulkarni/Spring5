033278a7bac5e0acd69fa619f6b48470e41e62f6
sumati-kulkarni <sumati.kulkarni.90@gmail.com>
2020-03-11 22:50:06 -0400

    Updating Repository

--- Spring5WebApp/src/main/java/com/test/controller/HomeController.java
+++ Spring5WebApp/src/main/java/com/test/controller/HomeController.java
@@ -1,15 +1,33 @@
 package com.test.controller;
 
+import com.test.model.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello")
     @ResponseBody
     public String welcome() {
-        return "Hello World from Web-App";
+        return "Hello World from Web-App..!!";
+    }
+
+    @RequestMapping(value = "/")
+    public String home() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/person")
+    public ModelAndView person(Model model) {
+        Person person = new Person();
+        person.setName("David");
+        person.setAge(25);
+        person.setPersonId(2);
+        model.addAttribute("person", person);
+        return new ModelAndView("index");
     }
 }
---
+++ Spring5WebApp/src/main/java/com/test/model/Person.java
@@ -0,0 +1,40 @@
+package com.test.model;
+
+public class Person {
+    private int personId;
+    private String name;
+    private int age;
+    private String password;
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
